ccb6361d2f2cd04726bf5a7f9a3ff5c7d3a0620f
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2018-10-18 00:36:17 +0300

    Close branches in DFS and make it efficient

--- lab1/SemanticTableaux.py
+++ lab1/SemanticTableaux.py
@@ -1,6 +1,6 @@
 from parser import GrammarTransformer
 from util import stringify
-from copy import deepcopy
+from copy import deepcopy, copy
 
 from representation.AndNode import AndNode
 from representation.OrNode import OrNode
@@ -14,12 +14,12 @@ class SemanticTableaux:
     def solve(self, filename):
         with open(filename, 'r') as content_file:
             content = content_file.read()
-        second = GrammarTransformer().transform(content)
+        root = GrammarTransformer().transform(content)
 
         results = {}
         results['satisfiable'] = False
 
-        self.dfs(second, results)
+        self.dfs(root, results)
 
         if results['satisfiable'] == True:
             print(filename, 'SATISFIABLE', stringify(results['example']))
@@ -27,143 +27,103 @@ class SemanticTableaux:
             print(filename, 'NON SATISFIABLE')
 
     def dfs(self, root, results):
+        # Stop if satisfiability is found
         if results['satisfiable'] == True:
             return
-        if (root.surely_not_satisfiable()):
-            return
+
+        # If there are no more unsolved nodes, we found a solution
         if len(root.unsolved_nodes) == 0:
             new_solution = list(map(lambda x: deepcopy(x), root.solved_nodes))
             results['satisfiable'] = True
             results['example'] = new_solution
             return
-        visited = []
+
+        # Search for unsolved nodes that create trunks
         for node in root.unsolved_nodes:
-            if node in visited:
-                continue
-            visited.append(node)
             if isinstance(node, VariableNode) or (
                 isinstance(node, NegationNode) and isinstance(
                     node.value, VariableNode
                 )
             ):
                 root.unsolved_nodes.remove(node)
-                root.solved_nodes.append(node)
-                self.dfs(root, results)
+                conflict = root.add_solved_node(node)
+                if conflict:
+                    return
 
-                root.solved_nodes.remove(node)
-                root.unsolved_nodes.append(node)
+                self.dfs(root, results)
+                return
             elif isinstance(node, AndNode):
+                # Try to add node's solved nodes to root
+                for solved_node in node.solved_nodes:
+                    conflict = root.add_solved_node(solved_node)
+                    if conflict:
+                        return
                 root.unsolved_nodes.remove(node)
                 root.unsolved_nodes.extend(node.unsolved_nodes)
-                root.solved_nodes.extend(node.solved_nodes)
-                self.dfs(root, results)
 
-                root.unsolved_nodes.append(node)
-                for unsolved_node in node.unsolved_nodes:
-                    root.unsolved_nodes.remove(unsolved_node)
-                for solved_node in node.solved_nodes:
-                    root.solved_nodes.remove(solved_node)
-            elif isinstance(node, OrNode):
+                self.dfs(root, results)
+                return
+            elif isinstance(node, NegationNode) and isinstance(node.value, OrNode):
+                root.unsolved_nodes.remove(node)
+                root.unsolved_nodes.extend(list(map(lambda x: NegationNode(x), node.value.child_nodes)))
+
+                self.dfs(root, results)
+                return
+            elif isinstance(node, NegationNode) and isinstance(node.value, ImplicationNode):
+                root.unsolved_nodes.remove(node)
+                root.unsolved_nodes.append(AndNode([node.value.p, NegationNode(node.value.q)]))
+                self.dfs(root, results)
+                return
+
+        for node in root.unsolved_nodes:
+            if isinstance(node, OrNode):
                 for or_node in node.child_nodes:
-                    root.unsolved_nodes.remove(node)
-                    root.unsolved_nodes.append(or_node)
-                    self.dfs(root, results)
-
-                    root.unsolved_nodes.remove(or_node)
-                    root.unsolved_nodes.append(node)
+                    new_root = root.deepcopy()
+                    new_root.unsolved_nodes.remove(node)
+                    new_root.unsolved_nodes.append(or_node)
+                    self.dfs(new_root, results)
+                return
             elif isinstance(node, ImplicationNode):
-                root.unsolved_nodes.remove(node)
-                neg_p = NegationNode(node.p)
-                root.unsolved_nodes.append(neg_p)
-                self.dfs(root, results)
+                left_root = root.deepcopy()
+                left_root.unsolved_nodes.remove(node)
+                left_root.unsolved_nodes.append(NegationNode(node.p))
+                self.dfs(left_root, results)
 
-                root.unsolved_nodes.append(node)
-                root.unsolved_nodes.remove(neg_p)
-
-                root.unsolved_nodes.remove(node)
-                root.unsolved_nodes.append(node.q)
-                self.dfs(root, results)
-
-                root.unsolved_nodes.append(node)
-                root.unsolved_nodes.remove(node.q)
+                right_root = root.deepcopy()
+                right_root.unsolved_nodes.remove(node)
+                right_root.unsolved_nodes.append(node.q)
+                self.dfs(right_root, results)
+                return
             elif isinstance(node, IffNode):
-                root.unsolved_nodes.remove(node)
-                p_and_q = AndNode([node.p, node.q])
-                root.unsolved_nodes.append(p_and_q)
-                self.dfs(root, results)
+                left_root = root.deepcopy()
+                left_root.unsolved_nodes.remove(node)
+                left_root.unsolved_nodes.append(AndNode([node.p, node.q]))
+                self.dfs(left_root, results)
 
-                root.unsolved_nodes.append(node)
-                root.unsolved_nodes.remove(p_and_q)
+                right_root = root.deepcopy()
+                right_root.unsolved_nodes.remove(node)
+                right_root.unsolved_nodes.append(AndNode(
+                    [NegationNode(node.p), NegationNode(node.q)]))
+                self.dfs(right_root, results)
+                return
+            elif isinstance(node, NegationNode) and isinstance(node.value, AndNode):
+                for and_node in node.value.solved_nodes + node.value.unsolved_nodes:
+                    new_root = root.deepcopy()
+                    new_root.unsolved_nodes.remove(node)
+                    new_root.unsolved_nodes.append(NegationNode(and_node))
+                    self.dfs(new_root, results)
+                return
+            elif isinstance(node, NegationNode) and isinstance(node.value, IffNode):
+                left_root = root.deepcopy()
+                left_root.unsolved_nodes.remove(node)
+                left_root.unsolved_nodes.append(AndNode([node.p, NegationNode(node.q)]))
+                self.dfs(left_root, results)
 
-                root.unsolved_nodes.remove(node)
-                non_p_and_non_q = AndNode(
-                    [NegationNode(node.p), NegationNode(node.q)])
-                root.unsolved_nodes.append(non_p_and_non_q)
-                self.dfs(root, results)
-
-                root.unsolved_nodes.append(node)
-                root.unsolved_nodes.remove(non_p_and_non_q)
-            elif isinstance(node, NegationNode):
-                if isinstance(node.value, AndNode):
-                    for and_node in node.value.solved_nodes:
-                        root.unsolved_nodes.remove(node)
-                        neg_node = NegationNode(and_node)
-                        root.unsolved_nodes.append(neg_node)
-                        self.dfs(root, results)
-
-                        root.unsolved_nodes.append(node)
-                        root.unsolved_nodes.remove(neg_node)
-                    for and_node in node.value.unsolved_nodes:
-                        root.unsolved_nodes.remove(node)
-                        neg_node = NegationNode(and_node)
-                        root.unsolved_nodes.append(NegationNode(and_node))
-                        self.dfs(root, results)
-
-                        root.unsolved_nodes.append(node)
-                        root.unsolved_nodes.remove(neg_node)
-                elif isinstance(node.value, OrNode):
-                    root.unsolved_nodes.remove(node)
-                    neg_nodes = []
-                    for or_node in node.value.child_nodes:
-                        neg_node = NegationNode(or_node)
-                        neg_nodes.append(neg_node)
-                        root.unsolved_nodes.append(neg_node)
-                    self.dfs(root, results)
-
-                    root.unsolved_nodes.append(node)
-                    for neg_node in neg_nodes:
-                        root.unsolved_nodes.remove(neg_node)
-                elif isinstance(node.value, ImplicationNode):
-                    root.unsolved_nodes.remove(node)
-                    root.unsolved_nodes.append(node.value.p)
-                    neg_q = NegationNode(node.value.q)
-                    root.unsolved_nodes.append(neg_q)
-                    self.dfs(root, results)
-
-                    root.unsolved_nodes.append(node)
-                    root.unsolved_nodes.remove(node.value.p)
-                    root.unsolved_nodes.remove(neg_q)
-                elif isinstance(node.value, IffNode):
-                    root.unsolved_nodes.remove(node)
-                    and_node = AndNode([
-                        node.value.p, NegationNode(node.value.q)
-                    ])
-                    root.unsolved_nodes.append(and_node)
-                    self.dfs(root, results)
-
-                    root.unsolved_nodes.append(node)
-                    root.unsolved_nodes.remove(and_node)
-
-                    root.unsolved_nodes.remove(node)
-                    and_node = AndNode([
-                        NegationNode(node.value.p), node.value.q
-                    ])
-                    root.unsolved_nodes.append(and_node)
-                    self.dfs(root, results)
-
-                    root.unsolved_nodes.append(node)
-                    root.unsolved_nodes.remove(and_node)
-                else:
-                    print("Unknown type of NegationNode value")
+                right_root = root.deepcopy()
+                right_root.unsolved_nodes.remove(node)
+                right_root.unsolved_nodes.append(AndNode([NegationNode(node.p), node.q]))
+                self.dfs(right_root, results)
+                return
             else:
                 print("Unknown type of AndNode child")
+                return
--- lab1/representation/AndNode.py
+++ lab1/representation/AndNode.py
@@ -1,32 +1,29 @@
-from copy import deepcopy
-
 from NegationNode import NegationNode
 from VariableNode import VariableNode
+
+from copy import deepcopy as dc
 
 class AndNode:
     solved_nodes = []
     unsolved_nodes = []
+    solved_variables = {}
 
     def __init__(self, child_nodes=[]):
-        self.solved_nodes = []
         self.unsolved_nodes = child_nodes
 
-    def surely_not_satisfiable(self):
-        for node_1 in self.solved_nodes:
-            for node_2 in self.solved_nodes:
-                if isinstance(
-                        node_1,
-                        VariableNode) and isinstance(
-                        node_2,
-                        NegationNode) and node_1.name == node_2.value.name:
-                    return True
-                if isinstance(
-                        node_2,
-                        VariableNode) and isinstance(
-                        node_1,
-                        NegationNode) and node_2.name == node_1.value.name:
-                    return True
+    def add_solved_node(self, node):
+        if self.solved_variables.get(str(NegationNode(node)), 0) > 0:
+            return True
+
+        self.solved_nodes.append(node)
+        str_repr = str(node)
+        self.solved_variables[str_repr] = self.solved_variables.get(str_repr, 0) + 1
         return False
+
+    def remove_solved_node(self, node):
+        self.solved_nodes.remove(node)
+        self.solved_variables[str(node)] -= 1
+
 
     def __str__(self):
         to_print = "AND("
@@ -36,3 +33,10 @@ class AndNode:
             to_print += str(node) + ", "
         to_print += ")"
         return to_print
+
+    def deepcopy(self):
+        new_node = AndNode()
+        new_node.solved_nodes = self.solved_nodes[:]
+        new_node.unsolved_nodes = self.unsolved_nodes[:]
+        new_node.solved_variables = dc(self.solved_variables)
+        return new_node
